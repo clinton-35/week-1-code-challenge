9988a4d4be28061fc85a851d7ee8f2dd425a1d81
clinton <clinton.wambua@student.moringaschool.com>
2023-03-21 11:41:46 +0300

    assignment complete

--- net salary calculator/script.js
+++ net salary calculator/script.js
@@ -7,8 +7,7 @@ const hospContributionInput = document.getElementById("hosp-contribution");
 const housingReliefInput = document.getElementById("housing-relief");
 const ownerOccupierInterestInput = document.getElementById("owner-occupier-interest");
 const disabilityExemptionInput = document.getElementById("disability-exemption");
-const nssfTier1 = document.getElementById("nssf-tier1").value;
-const nssfTier2 = document.getElementById("nssf-tier2").value;
+
 
 // Get the submit button and result output element
 const calculateButton = document.getElementById("calculate-button");
@@ -87,19 +86,20 @@ function calculateTax() {
       break;
     }
   }
-  let nssf = 0;
+
+  // calculating nssf tax
+  let nssf;
   if (basicSalary <= 6000) {
     nssf = basicSalary * 0.06;
-  } else if (basicSalary <= 18000) {
-    nssf = (6000 * 0.06) + ((basicSalary - 6000) * 0.06);
   } else {
     nssf = (6000 * 0.06) + (12000 * 0.06);
   }
 
-  const tax = basicSalary - nssf;
+  const tax = basicSalary - nssf- totalTax- nhifContribution; 
+  
   
   // Display the result
-  resultOutput.textContent = `Your PAYE tax is: ${totalTax.toFixed(2)}, your NHIF contribution is: ${nhifContribution}  and your nssf is:${tax.toFixed(2)} `;
+  resultOutput.textContent = `Your PAYE tax is: ${totalTax.toFixed(2)}, your NHIF contribution is: ${nhifContribution} , your nssf is:${nssf} and your salary is:${tax.toFixed(2)}`;
 }
 
 // Add event listener to the submit button
--- sudent grade generator/script.js
+++ sudent grade generator/script.js
@@ -5,29 +5,25 @@ function show_result() {
     let num2 = document.querySelector("#js").value;
     let num3 = document.querySelector("#css").value;
     // calculating total and percentege
-    let to = parseFloat(num) + parseFloat(num2) + parseFloat(num3);
-    let per = (to * 100) / 300;
+    let total = parseFloat(num) + parseFloat(num2) + parseFloat(num3);
+    let percentage = (total * 100) / 300;
     // grading of the percentageusing if....else statement
-    if (per < 40) {
+    if (percentage < 40) {
       document.querySelector(".gra").innerHTML = "E";
-    } else if (per >= 40 && per <= 49) {
+    } else if (percentage >= 40 && percentage <= 49) {
       document.querySelector(".gra").innerHTML = "D";
-    } else if (per >= 50 && per <= 59) {
+    } else if (percentage >= 50 && percentage <= 59) {
       document.querySelector(".gra").innerHTML = "C";
-    } else if (per >= 60 && per <= 79) {
+    } else if (percentage >= 60 && percentage <= 79) {
       document.querySelector(".gra").innerHTML = "B";
-    } else if (per >= 80 && per <= 100) {
+    } else if (percentage >= 80 && percentage <= 100) {
       document.querySelector(".gra").innerHTML = "A";
     } else {
       document.querySelector(".gra").innerHTML = "Invalid";
     }
     // assigning the  percentage annd total results to the html file
-    document.querySelector(".to").innerHTML = to;
-    document.querySelector(".per").innerHTML = per.toFixed(2);
+    document.querySelector(".to").innerHTML = total;
+    document.querySelector(".per").innerHTML = percentage.toFixed(2);
     
-    if (per < 40) {
-      document.querySelector(".result h2").innerHTML = "You have failed!";
-    } else {
-      document.querySelector(".result h2").innerHTML = "You have passed!";
-    }
-  }
+    
+}
